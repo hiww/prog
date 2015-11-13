9bbbd9b1abf6b8c16c4231e7b74ace489c1923b5
hiww <sony3150gundam@gmail.com>
2015-11-14 02:39:48 +0900

    Update heap.c

--- heap.c
+++ heap.c
@@ -2,14 +2,14 @@
 #define LEFT(i) (2 * i)
 #define RIGHT(i) (2 * i + 1)
 #define N 10
-void Build_Tree(int*, int);
-void Max_Heapify(int*, int, int);
-void Build_Max_Heap(int*, int);
+void HEAPSORT(int*, int);
+void MAX_HEAPIFY(int*, int, int);
+void BUILD_MAX_HEAP(int*, int);
 
 int main(void) {
 	int i, j, n = N, tmp, A[] = {4,1,3,2,16,9,10,14,8,7};
 	for (i = 0; i < N - 1; i++) {
-		Build_Tree(A, n);
+		HEAPSORT(A, n);
 		n--;
 		tmp = A[0];
 		A[0] = A[n];
@@ -22,15 +22,15 @@ int main(void) {
 	return 0;
 }
 
-void Build_Tree(int A[], int n) {
+void HEAPSORT(int A[], int n) {
 	int i;
-	Build_Max_Heap(A, n);
+	BUILD_MAX_HEAP(A, n);
 	for (i = n; i > 0; i--) {
-		Max_Heapify(A, i, n);
+		MAX_HEAPIFY(A, i, n);
 	}
 }
 
-void Max_Heapify(int A[], int i, int n) {
+void MAX_HEAPIFY(int A[], int i, int n) {
 	int l = LEFT(i), r = RIGHT(i), largest, tmp;
 	if (l <= n && A[l - 1] > A[i - 1]) {
 		largest = l;
@@ -44,13 +44,13 @@ void Max_Heapify(int A[], int i, int n) {
 		tmp = A[i - 1];
 		A[i - 1] = A[largest - 1];
 		A[largest - 1] = tmp;
-		Max_Heapify(A, largest, n);
+		MAX_HEAPIFY(A, largest, n);
 	}
 }
 
-void Build_Max_Heap(int A[], int n){
+void BUILD_MAX_HEAP(int A[], int n){
 	int i;
 	for (i = n / 2; i > 0; i--) {
-		Max_Heapify(A, i, n);
+		MAX_HEAPIFY(A, i, n);
 	}
 }
