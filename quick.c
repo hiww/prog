7a7536a1b805aeba1f6f082b2653e03adba26ab1
hiww <sony3150gundam@gmail.com>
2015-11-19 14:17:19 +0900

    Update quick.c

--- quick.c
+++ quick.c
@@ -1,4 +1,48 @@
 #include <stdio.h>
-...
-j
-11
+
+void swap(int* i, int* j) {
+	int k;
+	k = *i;
+	*i = *j;
+	*j = k;
+}
+void QuickSort(int data[], int left, int right) {
+	int x = left, y = right;
+	int base = data[(left + right) / 2];
+	while (1) {
+		while (data[x] < base) {
+			x++;
+		}
+		while (data[y] > base) {
+			y--;
+		}
+		/* 重なるとbreak */
+		if (x >= y) {
+			break;
+		}
+		/* 見つけた値の入れ替え */
+		swap(&data[x], &data[y]);
+		x++;
+		y++;
+	}
+	/* 左右の要素がまだ分割可能ならソート */
+	if (left < x - 1) {
+		QuickSort(data, left, x - 1);
+	}
+	if (right > y + 1) {
+		QuickSort(data, y + 1, right);
+	}
+}
+int main (void) {
+	int i, n, A[] = {1, 30, 2, 11, 3, 5, 12, 0, 4, 6};
+	/* 配列の長さを求める */
+	n = sizeof(A) / sizeof(int);
+	/* クイックソートをする */
+	QuickSort(A, 0, n);	
+	/* 結果の表示をする */
+	for (i = 0; i < n; i++) {
+		printf("%d ", A[i]);
+	}
+	printf("\n");
+	return 0;
+}
