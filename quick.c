791525027626735f01a468ef18475f99385a6b4f
hiww <sony3150gundam@gmail.com>
2015-11-19 16:35:21 +0900

    Update quick.c

--- quick.c
+++ quick.c
@@ -26,18 +26,21 @@ void QuickSort(int data[], int left, int right) {
 		y--;
 	}
 	/* 左右の要素がまだ分割可能ならソート */
-	if (left < x - 1) QuickSort(data, left, x - 1);
-	if (right > y + 1) QuickSort(data, y + 1, right);
+	if (left < x - 1) {
+		QuickSort(data, left, x - 1);
+	}
+	if (right > y + 1) {
+		QuickSort(data, y + 1, right);
+	}
 }
 int main(void) {
-	int i, n, N, A[] = {2, 1, 3};
+	int i, n, A[] = {2, 1, 3, 5, 5, 76, 56, 34, 54};
 	/* 配列の長さを求める */
-	N = sizeof(A) / sizeof(int);
-	n = N;
+	n = sizeof(A) / sizeof(int);
 	/* クイックソートをする */
-	QuickSort(A, 0, n);	
+	QuickSort(A, 0, n - 1);
 	/* 結果の表示をする */
-	for (i = 1; i <= N; i++) {
+	for (i = 0; i < n; i++) {
 		printf("%d ", A[i]);
 	}
 	printf("\n");
